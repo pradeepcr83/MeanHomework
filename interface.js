b0c4053a3d75647fcde546c3e3a5c6d337b0b708
pradeep.cr <pradeep.cr@nestgroup.net>
2016-08-23 19:10:29 +0530

    These TCS PASS. -------------- insert documents. query for one document. query for more than one and sort in ascending order.
    HOMEWORK 1 DONE

--- interface.js
+++ interface.js
@@ -13,38 +13,12 @@ exports.insert = function(db, doc, callback) {
  *  ordered by the movie's "title" field. See
  *  http://mongodb.github.io/node-mongodb-native/2.0/api/Cursor.html#sort
  */
-/*db.messages.find({ headers : { From: "reservations@marriott.com" } })
-
-This queries for documents where headers equals { From: ... }, i.e. contains no other fields.
-
-db.messages.find( { 'headers.From': "reservations@marriott.com" }  )
-
-This only looks at the headers.From field, not affected by other fields contained in, or missing from, headers.*/
 var retDoc = [];
 exports.byDirector = function (db, director, callback) {
     // TODO: implement
     var query = { 'director': director };
-    
     console.log(query);
-    var moviesByHim = db.collection('movies').find(query , (function (err, doc) {
-        doc.toArray(callback);
-        
-    }));
-    /*var moviesByHim = db.collection('movies').find(query).toArray(function (err, doc) {
-
-        doc.forEach(function (d) {
-            console.log("77777777777777");
-            console.log(JSON.stringify(d));
-            retDoc.push(JSON.stringify(d));
-            console.log("8888888888" + retDoc);
-            //callback(null, retDoc);
-        })
-    });*/
-    //callback(null, [moviesByHim]);
-    /*retDoc.forEach(function (dq) {
-        console.log("888888888888");
-        console.log(JSON.stringify(dq));        
-    })*/
-    //console.log("8888888888" + retDoc);
-    //callback(null, [retDoc]);
+    db.collection('movies').find(query , (function (err, doc) {        
+        doc.sort({ 'title': 1 }).toArray(callback);        
+    }));  
 };
